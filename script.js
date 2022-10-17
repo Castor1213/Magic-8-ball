4faf32752a1fda3ebcbb2049391b8a1b5ba11514
Wojtek <bober.developer@gmail.com>
2022-10-17 13:11:18 +0200

    adding an array and retrieving the necessary elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const ball = document.querySelector('.ball-img')
+const answer = document.querySelector('.answer')
+const error = document.querySelector('.error')
+const input = document.querySelector('input')
+
+const answersArr = ['Tak!', 'Nie', 'Może', 'Ciężko powiedzieć', 'Lepiej byś nie znał odpowiedzi na to pytanie ...']
+
